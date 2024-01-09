fe518d0ee2080310b844a0fc7701e62785659225
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-09 14:52:45 +0530

    Add files via upload

---
+++ VoucherTrackerApp/DAL/Interface/IVoucherTrackerInterface.cs
@@ -0,0 +1,19 @@
+﻿using VoucherTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoucherTrackerApp.DAL.Interface
+{
+    public interface IVoucherTrackerInterface 
+    {
+        IEnumerable<Voucher> GetVouchers();
+        Voucher GetVoucherByID(int voucherId);
+        Voucher InsertVoucher(Voucher voucher);
+        int DeleteVoucher(int voucherId);
+        bool UpdateVoucher(Voucher voucher);
+        void Save();
+    }
+}
---
+++ VoucherTrackerApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VoucherTrackerApp.MvcApplication" Language="C#" %>
